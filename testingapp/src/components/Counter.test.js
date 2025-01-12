2e142a6521a5a51b8bcdb8022641ef386c8cc7f7
Mohad009 <26S2053@utas.edu.om>
2025-01-12 14:54:53 +0400

    class activity 9&10

--- testingapp/src/components/Counter.test.js
+++ testingapp/src/components/Counter.test.js
@@ -2,3 +2,19 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 import Counter from "./Counter";
+
+
+describe(Counter, () => {
+    it("counter displays correct initial count", () => {  
+        render(<Counter initialCount={5}/>)
+    })
+it("Increments the count when the increment button is clicked",()=>{
+    render(<Counter initialCount={5}/>)
+    const h3element=screen.getByTestId("count")
+    const btnelement=screen.getByTestId("increment")
+    fireEvent.click(btnelement)
+    expect(h3element).toHaveTextContent(6)
+})
+
+
+ })
